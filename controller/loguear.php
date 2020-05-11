265aad9a4be2154aa60eae0b167d1f44556e3ecc
Diter  Navarrete <diternavarrete@MacBook-Pro-de-Diter.local>
2020-05-11 13:02:47 -0400

    href a videollamada

--- controller/loguear.php
+++ controller/loguear.php
@@ -2,9 +2,10 @@
 
 include '../bd/conexion.php';
 if($_SERVER['REQUEST_METHOD']=="POST"){
-$username=$_POST['username'];
+$username=$_POST['username'];    
 $password=$_POST['password'];
-$query="SELECT * FROM paciente.usuario where usuario='$username' and contrasena='$password'";
+
+$query="SELECT * FROM usuario where usuario='$username' and contrasena='$password'";
 $res=pg_query($con,$query);
 $rows = pg_num_rows($res);
 $data = pg_fetch_array($res);    
@@ -12,6 +13,7 @@ session_start();
  if($rows==1){
  	header('location:../home.php');
  	$_SESSION['username']= $data['usuario'];
+    $id=$data['id'];
  }
 else{
 	echo "Contraseña incorrecta";
--- php/carga-div-video.php
+++ php/carga-div-video.php
@@ -1,14 +1,20 @@
 <?php
 include '../bd/conexion.php';
 //aca se pondra un estado de base de datos
-$user="admin";
-$pas="123456";
-$query="SELECT * FROM paciente.usuario where usuario='$user' and contrasena='$pas'";
-$res=pg_query($con,$query);
-$rows = pg_num_rows($res);
-$data = pg_fetch_array($res);    
 session_start();
- if(!empty($data['usuario'])){
+$user=$_SESSION['username'];
+
+$queryid="select id from usuario where usuario ='$user'";
+$resid=pg_query($con,$queryid);
+$dataid = pg_fetch_array($resid);
+$id=$dataid['id'];
+
+$queryv="SELECT * FROM videousuario where id_usuario='$id'";
+$resv=pg_query($con,$queryv);
+$rowsv = pg_num_rows($resv);
+$datav = pg_fetch_array($resv);    
+
+ if($datav['estado'] == 1){
 echo ' 
                     <div class="our-team">
                         <div class="pic">
@@ -18,7 +24,7 @@ echo '
                             <h3 class="title">Doctor</h3>
                             <span class="title">Ir a video llamada</span>
                             <ul class="social">
-                                <li><a href="" title="camara desbloqueada" onclick="recargar()"><i class="fa fa-video-camera" aria-hidden="true"></i></a></li>
+                                <li><a href="http://videoapsqa.boxvirtual.cl/apsvideoapi/meetings?id='.$id.'&width=700&heigth=500 " title="camara desbloqueada" onclick="recargar()" target="_blank"><i class="fa fa-video-camera" aria-hidden="true"></i></a></li>
                                 
                             </ul>
                         </div>
